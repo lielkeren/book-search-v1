3f1f39a5ff269bc61cddd1b8358252485cf625a9
Liel Keren <lielkeren.lk@gmail.com>
2023-09-30 12:03:44 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as bookData from "../assets/books.json";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BookLocator';
+  searchTerm: string = '';
+  searchResults: any[] = [];
+
+  // Sample data. In a real-world scenario, you'd probably fetch this from an API or service.
+  books: any[] = (bookData as any).default;
+
+  searchBooks() {
+    console.log('liel ');
+
+    console.log(this.books);
+
+    this.searchResults = this.books.filter(book =>
+      book.bookname.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
